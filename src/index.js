759b16f86f02db890ad103d3cfe7dc8db32a4a2f
Ollie Moss <olliemoss321@gmail.com>
2024-07-15 09:17:31 +1200

    Initalized files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+import ErrorPage from './error-page';
+import Dashboard from './dashboard';
+import Courses from './courses';
+import Assignments from './assignments';
+import Login from './login';
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Root</div>,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: "auth",
+        element: <Login />,
+    },
+    {
+        path: "dashboard",
+        element: <Dashboard />,
+    },
+    {
+        path: "courses",
+        element: <Courses />, 
+    },
+    {
+        path: "assignments",
+        element: <Assignments />,
+    },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/login.js
@@ -0,0 +1,110 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+import { useAuthState, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useState } from 'react';
+
+firebase.initializeApp({
+    apiKey: "***REMOVED***",
+    authDomain: "***REMOVED***",
+    projectId: "***REMOVED***",
+    storageBucket: "***REMOVED***.appspot.com",
+    messagingSenderId: "***REMOVED***",
+    appId: "1:***REMOVED***:web:***REMOVED***",
+    measurementId: "***REMOVED***"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+function Login() {
+    const [user] = useAuthState(auth);
+
+    return (
+        <div className='flex flex-col items-center'>
+            <h1 className="text-3xl font-bold">Login</h1>
+
+            <section>
+                {user ? <LoggedIn /> : <LogIn />}
+            </section>
+        </div>
+    );
+}
+
+function LoggedIn() {
+    return (
+        <div>
+            <Logout/>
+        </div>
+    );
+}
+
+function LogIn(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const signInWithEmailAndPass = () => {
+        auth.signInWithEmailAndPassword(email, password).catch((error) => {
+            var errorCode = error.code;
+
+            switch(errorCode){
+                case "auth/user-disabled":
+                    setError("This account has been disabled!");
+                    break;
+                case "auth/user-not-found":
+                    setError("There is no account associated with this email!");
+                    break;
+                case "auth/invalid-email":
+                case "auth/wrong-password":
+                    setError("Email or password is incorrect please try again!");
+                    break
+                default:
+                    
+            }
+        });
+    };
+    const signInWithGoogle = () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        auth.signInWithPopup(provider).then((result) => {
+            var user = result.user;
+
+            var credential = result.credential;
+            
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+    return (
+        <div className='flex flex-col items-start'>
+        <p>{error}</p>
+            <label>
+                Email: <input 
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        />
+            </label>
+            <label>
+                Password: <input 
+                            type='password'
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                            />
+            </label>
+            <div className='flex justify-items-center'>
+                <button className='border-solid border-2' onClick={signInWithEmailAndPass}>Sign In</button>
+                <button onClick={signInWithGoogle}>or Sign In with Google</button>
+            </div>
+        </div>
+    );
+}
+
+function Logout(){
+    return auth.currentUser && (
+        <button onClick={() => auth.signOut()}>Sign Out</button>
+    );
+}
+export default Login;
